0efa02a2924e02a31ca0d71f67ab868091bf7fd6
Lázaro Vanderson <eulazzo@protonmail.com>
2022-08-28 19:54:08 -0300

    codewars challenge

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace APOO
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      string word = Console.ReadLine();
+      Console.WriteLine($"{word}");
+
+      string result = WordWithoutTwoCaracter(word);
+    }
+
+    static string WordWithoutTwoCaracter(string word)
+    {
+      return word.Substring(1, word.Length - 2);
+    }
+  }
+}
