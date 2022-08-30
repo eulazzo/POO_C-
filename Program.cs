9b5d9c75205448675b5deec3433c2a68b8cde35e
Lázaro Vanderson <eulazzo@protonmail.com>
2022-08-29 21:50:05 -0300

    person class exercise

--- Program.cs
+++ Program.cs
@@ -6,9 +6,35 @@ namespace APOO
   {
     public static void Main(string[] args)
     {
-      
+
+      Person person_1 = new Person();
+      Person person_2 = new Person();
+
+      Console.WriteLine("First Person Data: ");
+      Console.Write("Name: ");
+      person_1.Name = Console.ReadLine();
+      Console.Write("Age: ");
+      person_1.Age = int.Parse(Console.ReadLine());
+
+      Console.Write("Second Person Data: ");
+
+      Console.Write("Name: ");
+      person_2.Name = Console.ReadLine();
+      Console.Write("Age: ");
+      person_2.Age = int.Parse(Console.ReadLine());
+
+      if (person_1.Age > person_2.Age)
+        Console.WriteLine($"Old Person: {person_1.Name}");
+      else
+        Console.WriteLine($"Old Person: {person_2.Name}");
     }
 
+    class Person
+    {
+      public string Name;
+      public int Age;
+
+    }
 
   }
 }
